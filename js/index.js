9740d348495f05bfb3d584b47319307ca679e7d0
shiwei1210 <379171631@qq.com>
2016-08-09 17:09:21 +0800

    a

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
 (function () {
 
     //点击产品标题展开产品列表
+    //$(".show-btn").on("click", function () {
+    //    $(this).find("em").toggleClass("on");
+    //    if ($(this).find("em").hasClass("on")) {
+    //        $(this).siblings(".product-list").addClass("visiable");
+    //    }
+    //    else {
+    //        $(this).siblings(".product-list").removeClass("visiable");
+    //    }
+    //});
     $(".show-btn").on("click", function () {
-        $(this).find("em").toggleClass("on");
-        if ($(this).find("em").hasClass("on")) {
-            $(this).siblings(".product-list").addClass("visiable");
-        }
-        else {
-            $(this).siblings(".product-list").removeClass("visiable");
-        }
+        $(this).next().stop().slideToggle(500).parent().siblings().children("div").slideUp(500);
     });
-
     var now = {row: 1, col: 1}, last = {row: 0, col: 0};
     const towards = {up: 1, right: 2, down: 3, left: 4};
     var isAnimating = false;
@@ -31,115 +33,115 @@
                 event.preventDefault();
             }, false);
             var _flag = document.getElementById('index');
-            if(_flag){
-                $(document).swipeUp(function () {
-                    if (isAnimating) return;
-                    last.row = now.row;
-                    last.col = now.col;
-                    var pageLen = $(".page").length;
-                    if (last.row != pageLen) {
-                        now.row = last.row + 1;
-                        now.col = 1;
-                        pageMove(towards.up);
-
-                    }
-
-                    if (last.row == pageLen - 1) {
-                        $('.trasfrm').removeClass('ds768')
-                    }
-                });
-
-                $(document).swipeDown(function () {
-                    if (isAnimating) return;
-                    last.row = now.row;
-                    last.col = now.col;
-                    if (last.row != 1) {
-                        now.row = last.row - 1;
-                        now.col = 1;
-                        pageMove(towards.down);
-                    }
-                    if (last.row == 5) {
-                        $('.trasfrm').addClass('ds768')
-                    }
-                });
-
-                $(document).swipeLeft(function () {
-                    if (isAnimating) return;
-                    last.row = now.row;
-                    last.col = now.col;
-                });
-
-                $(document).swipeRight(function () {
-                    if (isAnimating) return;
-                    last.row = now.row;
-                    last.col = now.col;
-                    //if (last.row>1 && last.row<5 && last.col==2) { now.row = last.row; now.col = 1; pageMove(towards.right);}
-                    //if (last.row!=1) { now.row = last.row-1; now.col = 1; pageMove(towards.right);}
-                });
-
-
-                $(document).on("gonext", function (e) {
-                    last.row = now.row;
-                    last.col = now.col;
-                    if (last.row != 5) {
-                        now.row = last.row + 1;
-                        now.col = 1;
-                        pageMove(towards.up);
-                    }
-                }).on("goprew", function (e) {
-                    last.row = now.row;
-                    last.col = now.col;
-                    if (last.row != 1) {
-                        now.row = last.row - 1;
-                        now.col = 1;
-                        pageMove(towards.down);
-                    }
-                });
-
-                function pageMove(tw) {
-                    var lastPage = ".page-" + last.row + "-" + last.col,
-                        nowPage = ".page-" + now.row + "-" + now.col;
-
-                    switch (tw) {
-                        case towards.up:
-                            outClass = 'pt-page-moveToTop';
-                            inClass = 'pt-page-moveFromBottom';
-                            break;
-                        case towards.right:
-                            outClass = 'pt-page-moveToRight';
-                            inClass = 'pt-page-moveFromLeft';
-                            break;
-                        case towards.down:
-                            outClass = 'pt-page-moveToBottom';
-                            inClass = 'pt-page-moveFromTop';
-                            break;
-                        case towards.left:
-                            outClass = 'pt-page-moveToLeft';
-                            inClass = 'pt-page-moveFromRight';
-                            break;
-                    }
-                    isAnimating = true;
-                    $(nowPage).removeClass("hide");
-
-                    $(lastPage).addClass(outClass);
-                    $(nowPage).addClass(inClass);
-
-                    setTimeout(function () {
-                        $(lastPage).removeClass('page-current');
-                        $(lastPage).removeClass(outClass);
-                        $(lastPage).addClass("hide");
-                        $(lastPage).find(".img").addClass("hide");
-                        $(lastPage).find(".divimg").addClass("hide");
-
-                        $(nowPage).addClass('page-current');
-                        $(nowPage).removeClass(inClass);
-                        $(nowPage).find(".img").removeClass("hide");
-                        $(nowPage).find(".divimg").removeClass("hide");
-
-                        isAnimating = false;
-                    }, 600);
-                }
-            }
+            //if(_flag){
+            //    $(document).swipeUp(function () {
+            //        if (isAnimating) return;
+            //        last.row = now.row;
+            //        last.col = now.col;
+            //        var pageLen = $(".page").length;
+            //        if (last.row != pageLen) {
+            //            now.row = last.row + 1;
+            //            now.col = 1;
+            //            pageMove(towards.up);
+            //
+            //        }
+            //
+            //        if (last.row == pageLen - 1) {
+            //            $('.trasfrm').removeClass('ds768')
+            //        }
+            //    });
+            //
+            //    $(document).swipeDown(function () {
+            //        if (isAnimating) return;
+            //        last.row = now.row;
+            //        last.col = now.col;
+            //        if (last.row != 1) {
+            //            now.row = last.row - 1;
+            //            now.col = 1;
+            //            pageMove(towards.down);
+            //        }
+            //        if (last.row == 5) {
+            //            $('.trasfrm').addClass('ds768')
+            //        }
+            //    });
+            //
+            //    $(document).swipeLeft(function () {
+            //        if (isAnimating) return;
+            //        last.row = now.row;
+            //        last.col = now.col;
+            //    });
+            //
+            //    $(document).swipeRight(function () {
+            //        if (isAnimating) return;
+            //        last.row = now.row;
+            //        last.col = now.col;
+            //        //if (last.row>1 && last.row<5 && last.col==2) { now.row = last.row; now.col = 1; pageMove(towards.right);}
+            //        //if (last.row!=1) { now.row = last.row-1; now.col = 1; pageMove(towards.right);}
+            //    });
+            //
+            //
+            //    $(document).on("gonext", function (e) {
+            //        last.row = now.row;
+            //        last.col = now.col;
+            //        if (last.row != 5) {
+            //            now.row = last.row + 1;
+            //            now.col = 1;
+            //            pageMove(towards.up);
+            //        }
+            //    }).on("goprew", function (e) {
+            //        last.row = now.row;
+            //        last.col = now.col;
+            //        if (last.row != 1) {
+            //            now.row = last.row - 1;
+            //            now.col = 1;
+            //            pageMove(towards.down);
+            //        }
+            //    });
+            //
+            //    function pageMove(tw) {
+            //        var lastPage = ".page-" + last.row + "-" + last.col,
+            //            nowPage = ".page-" + now.row + "-" + now.col;
+            //
+            //        switch (tw) {
+            //            case towards.up:
+            //                outClass = 'pt-page-moveToTop';
+            //                inClass = 'pt-page-moveFromBottom';
+            //                break;
+            //            case towards.right:
+            //                outClass = 'pt-page-moveToRight';
+            //                inClass = 'pt-page-moveFromLeft';
+            //                break;
+            //            case towards.down:
+            //                outClass = 'pt-page-moveToBottom';
+            //                inClass = 'pt-page-moveFromTop';
+            //                break;
+            //            case towards.left:
+            //                outClass = 'pt-page-moveToLeft';
+            //                inClass = 'pt-page-moveFromRight';
+            //                break;
+            //        }
+            //        isAnimating = true;
+            //        $(nowPage).removeClass("hide");
+            //
+            //        $(lastPage).addClass(outClass);
+            //        $(nowPage).addClass(inClass);
+            //
+            //        setTimeout(function () {
+            //            $(lastPage).removeClass('page-current');
+            //            $(lastPage).removeClass(outClass);
+            //            $(lastPage).addClass("hide");
+            //            $(lastPage).find(".img").addClass("hide");
+            //            $(lastPage).find(".divimg").addClass("hide");
+            //
+            //            $(nowPage).addClass('page-current');
+            //            $(nowPage).removeClass(inClass);
+            //            $(nowPage).find(".img").removeClass("hide");
+            //            $(nowPage).find(".divimg").removeClass("hide");
+            //
+            //            isAnimating = false;
+            //        }, 600);
+            //    }
+            //}
         }
     }
     setTimeout(function(){
